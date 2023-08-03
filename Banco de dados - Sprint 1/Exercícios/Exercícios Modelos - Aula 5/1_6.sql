e3ec6364420e856003c7fbfe26491a6cb4986d51
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2023-08-03 00:02:18 -0300

    DDL finalizado

---
+++ Banco de dados - Sprint 1/Exercícios/Exercícios Modelos - Aula 5/1_6.sql
@@ -0,0 +1,47 @@
+-- DDL Exercicio 1_6
+
+CREATE TABLE Tipo
+(
+	IdTipo INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Equipamento
+(
+	IdEquipamento INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(30) NOT NULL,
+	Defeito VARCHAR(8000)
+);
+
+CREATE TABLE Cliente
+(
+	IdCliente INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(50) NOT NULL,
+	Email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Colaborador
+(
+	IdColaborador INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(50) NOT NULL,
+	Expediente VARCHAR(12) NOT NULL,
+	Salario DECIMAL NOT NULL
+);
+
+CREATE TABLE Servico
+(
+	IdServico INT PRIMARY KEY IDENTITY,
+	IdTipo INT FOREIGN KEY REFERENCES Tipo(IdTipo) NOT NULL,
+	IdCliente INT FOREIGN KEY REFERENCES Cliente(IdCliente) NOT NULL,
+	IdEquipamento INT FOREIGN KEY REFERENCES Equipamento(IdEquipamento) NOT NULL,
+	ValorTotal DECIMAL NOT NULL,
+	[Data] DATE NOT NULL
+);
+
+
+CREATE TABLE ColaboradorServico
+(
+	IdColaboradorServico INT PRIMARY KEY IDENTITY,
+	IdColaborador INT FOREIGN KEY REFERENCES Colaborador(IdColaborador) NOT NULL,
+	IdServico INT FOREIGN KEY REFERENCES Servico(IdServico) NOT NULL,
+);
