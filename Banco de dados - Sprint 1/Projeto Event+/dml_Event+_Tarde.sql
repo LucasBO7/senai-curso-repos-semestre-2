ee45ea7d6d693452ba6bafbccf2f99c03c62d9fc
Lucas Bianchezzi Oliveira <lucas.oliveira308@aluno.senai.br>
2023-08-10 17:16:37 -0300

    Finalizado

--- Banco de dados - Sprint 1/Projeto Event+/dml_Event+_Tarde.sql
+++ Banco de dados - Sprint 1/Projeto Event+/dml_Event+_Tarde.sql
@@ -4,26 +4,29 @@ USE [Event+_Tarde];
 
 -- Inserção de dados nas tabelas
 
+/*
 -- TiposDeUsuario
---INSERT INTO TiposDeUsuario(TituloTipoUsuario) VALUES('Administrador'), ('Comum');
+INSERT INTO TiposDeUsuario(TituloTipoUsuario) VALUES('Administrador'), ('Comum');
 
 -- TiposDeEvento
---INSERT INTO TiposDeEvento(TituloTipoEvento) VALUES('Palestra sobre SQL Server');
+INSERT INTO TiposDeEvento(TituloTipoEvento) VALUES('Palestra sobre SQL Server');
 
 -- Instituicao
---INSERT INTO Instituicao(CNPJ, Endereco, NomeFantasia) VALUES('13689012000117', 'Rua Niterói, 180 - São Caetano do Sul', 'Dev School');
+INSERT INTO Instituicao(CNPJ, Endereco, NomeFantasia) VALUES('13689012000117', 'Rua Niterói, 180 - São Caetano do Sul', 'Dev School');
 
 -- Usuario
---INSERT INTO Usuario(IdTipoDeUsuario, Nome, Email, Senha) VALUES(1, 'Lucas', 'lucas@gmail.com', 'lucas123');
+INSERT INTO Usuario(IdTipoDeUsuario, Nome, Email, Senha) VALUES(1, 'Lucas', 'lucas@gmail.com', 'lucas123');
+INSERT INTO Usuario(IdTipoDeUsuario, Nome, Email, Senha) VALUES(1, 'LucasAdmin', 'lucas@admin.com', 'LucasAdmin123');
 
 -- Evento
---INSERT INTO Evento(IdTipoDeEvento, IdInstituicao, Nome, Descricao, DataEvento, HorarioEvento) VALUES(1, 2, 'SQL Server', 'Palestra sobre SQL Server e sua importância nos sistemas.', CONVERT(DATE, '2023-08-10'), CONVERT(TIME, '10:30:00'));
+INSERT INTO Evento(IdTipoDeEvento, IdInstituicao, Nome, Descricao, DataEvento, HorarioEvento) VALUES(1, 2, 'SQL Server', 'Palestra sobre SQL Server e sua importância nos sistemas.', '2023-08-10', '10:30:00');
+*/
 
 -- PresencasEvento
---INSERT INTO PresencasEvento(IdUsuario, IdEvento, Situacao) VALUES(1, 2, 0);
+INSERT INTO PresencasEvento(IdUsuario, IdEvento) VALUES(1, 2);
 
 -- Comentario
---INSERT INTO Comentario(IdUsuario, IdEvento, Descricao, Exibe) VALUES(1, 2, 'Hoje abriremos as entradas para a palestra!', 0);
+INSERT INTO Comentario(IdUsuario, IdEvento, Descricao) VALUES(1, 2, 'Hoje abriremos as entradas para a palestra!');
 
 SELECT * FROM Usuario;
 SELECT * FROM Evento;
